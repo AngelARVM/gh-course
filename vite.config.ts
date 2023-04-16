5da73edb02fc36429a0d158bb478d18534fa73b0
AngelARVM <angelarvm@gmail.com>
2023-04-16 16:53:48 -0300

    change base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://angelarvm.github.io/',
+  base: '/gh-course',
   plugins: [react()],
 })
